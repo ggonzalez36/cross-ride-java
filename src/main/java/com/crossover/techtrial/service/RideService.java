3b8a7a52e0eaec0d60ee509ff2e8ead8698c6eff
ggonzalez36 <16825489+ggonzalez36@users.noreply.github.com>
2018-09-08 19:55:38 -0500

    implementation
    Implementation of  testing case, creation of the service findtopdrivres and  implementation

--- src/main/java/com/crossover/techtrial/service/RideService.java
+++ src/main/java/com/crossover/techtrial/service/RideService.java
@@ -15,6 +15,16 @@ public interface RideService {
   public Ride save(Ride ride);
   
   public Ride findById(Long rideId);
+
+  List<Ride> getAll();
+	/**
+	 * 
+	 * @param count
+	 * @param startTime
+	 * @param endTime
+	 * @return
+	 */
+	List<TopDriverDTO> findTopDrivers(long count, LocalDateTime startTime, LocalDateTime endTime);
   
 
 }
--- src/main/java/com/crossover/techtrial/service/RideServiceImpl.java
+++ src/main/java/com/crossover/techtrial/service/RideServiceImpl.java
@@ -3,11 +3,25 @@
  */
 package com.crossover.techtrial.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.crossover.techtrial.dto.TopDriverDTO;
+import com.crossover.techtrial.model.Person;
 import com.crossover.techtrial.model.Ride;
 import com.crossover.techtrial.repositories.RideRepository;
+import validation.RideValidation;
+
+
 
 /**
  * @author crossover
@@ -18,8 +32,18 @@ public class RideServiceImpl implements RideService{
 
   @Autowired
   RideRepository rideRepository;
+
+  @Autowired
+	PersonService personService;
   
   public Ride save(Ride ride) {
+
+
+    // Check that  driver or rider are already registered and validate time of ride 
+    if (personService.findById(ride.getDriver().getId()) == null || personService.findById(ride.getRider().getId()) == null || !RideValidation.validateRideTimes(ride))
+			return null;
+
+
     return rideRepository.save(ride);
   }
   
@@ -29,5 +53,116 @@ public class RideServiceImpl implements RideService{
       return optionalRide.get();
     }else return null;
   }
+
+  @Override
+	public List<Ride> getAll() {
+		List<Ride> rideList = new ArrayList<>();
+		rideRepository.findAll().forEach(rideList::add);
+		return rideList;
+
+  }
+  
+  @Override
+	public List<TopDriverDTO> findTopDrivers(long count, LocalDateTime startTime, LocalDateTime endTime) {
+
+		// Check rides which start and end within the mentioned durations
+		List<Ride> rides = getAll().stream()
+				.filter(ride -> (LocalDateTime.parse(ride.getStartTime()).equals(startTime)
+						|| LocalDateTime.parse(ride.getStartTime()).isAfter(startTime))
+						&& (LocalDateTime.parse(ride.getEndTime()).equals(endTime)
+								|| LocalDateTime.parse(ride.getEndTime()).isBefore(endTime)))
+				.collect(Collectors.toList());
+
+		// initialize map of driver and its rides info
+		Map<Person, DriverRide> driversMap = new HashMap<>();
+
+		for (Ride ride : rides) {
+
+			// Check to add rides to its driver
+			if (driversMap.containsKey(ride.getDriver())) {
+				driversMap.get(ride.getDriver()).addRide(ride);
+			} else {
+				DriverRide driverRides = new DriverRide(ride.getDriver());
+				// update ride info
+				driverRides.addRide(ride);
+				driversMap.put(ride.getDriver(), driverRides);
+			}
+		}
+
+		Set<DriverRide> driverRides = new TreeSet<>(driversMap.values());
+
+		// initialize result list
+		List<TopDriverDTO> result = new ArrayList<>();
+
+		// get only Top 5 drivers with max total duration value and calculate
+		// their average
+		for (DriverRide driverRide : driverRides.stream().limit(count).collect(Collectors.toList())) {
+			result.add(new TopDriverDTO(driverRide.getDriver().getName(), driverRide.getDriver().getEmail(),
+					driverRide.getTotalRideDurationInSeconds(), driverRide.getMaxRideDurationInSecods(),
+					driverRide.getAverageDistance()));
+
+		}
+
+		return result;
+	}
+
+	
+	class DriverRide implements Comparable<DriverRide> {
+
+		private Person driver;
+		private Set<Ride> rides;
+		private long totalRideDurationInSeconds;
+		private long maxRideDurationInSecods;
+		private long totalDistance;
+
+		public DriverRide(Person driver) {
+			this.driver = driver;
+			this.rides = new HashSet<>();
+		}
+
+		public Person getDriver() {
+			return driver;
+		}
+
+		public long getTotalRideDurationInSeconds() {
+			return totalRideDurationInSeconds;
+		}
+
+		public long getMaxRideDurationInSecods() {
+			return maxRideDurationInSecods;
+		}
+
+		public double getAverageDistance() {
+			return (double) totalDistance / rides.size();
+		}
+
+		public void addRide(Ride ride) {
+
+			rides.add(ride);
+
+			long rideDuration = ride.getRideDurationInSeconds();
+
+			if (rideDuration > maxRideDurationInSecods)
+				maxRideDurationInSecods = rideDuration;
+
+			totalRideDurationInSeconds += rideDuration;
+
+			totalDistance += ride.getDistance();
+		}
+
+		/**
+		 * to compare between total rides duration
+		 */
+		@Override
+		public int compareTo(DriverRide driverRides) {
+
+			if (this.totalRideDurationInSeconds > driverRides.totalRideDurationInSeconds) {
+				return 1;
+			} else if (this.totalRideDurationInSeconds < driverRides.totalRideDurationInSeconds) {
+				return -1;
+			}
+			return 0;
+		}
+	}
 
 }
